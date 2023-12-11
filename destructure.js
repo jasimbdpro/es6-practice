42c60b0bb37bd8f6b93158d291410e23b20de0c5
jasimbdpro <jasimbdpro@gmail.com>
2023-12-12 03:33:59 +0600

    change

---
+++ destructure.js
@@ -0,0 +1,8 @@
+const person = { names: 'Jack William', age: 17, job: 'facebooker', gfName: 'Ema Watson', phone: '01756892244', Friend: ['Tom Hack', 'John Yates'] }
+
+
+const { names, age, job } = person
+console.log(person.gfName, age, job, names)
+// const friends = ['Sakib', 'Mina', 'Runa', 'Mita']
+// const [first, second, ...rest] = friends;
+// console.log(second)
