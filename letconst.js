8ffb8439ddeb81802252097f99eed8316acef83c
jasimbdpro <jasimbdpro@gmail.com>
2023-12-11 14:18:43 +0600

    change

---
+++ letconst.js
@@ -0,0 +1,16 @@
+const hubby = 'Elius Kanchon';
+// console.log(hubby);
+
+const numbers = [12, 45]
+numbers[1] = 88;
+numbers.push(55)
+// console.log(numbers)
+let patientName = 'Abdur Rohim chaha'
+patientName = 'Jasim Uddin'
+patientName = 'Rittik chatai'
+console.log(patientName)
+let sum = 0
+for (let i = 0; i < 10; i++) {
+    sum + i;
+}
+console.log(i)
