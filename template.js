421f97669a8e7cb96a940801769957cd6788e351
jasimbdpro <jasimbdpro@gmail.com>
2023-12-11 22:56:58 +0600

    change

---
+++ template.js
@@ -0,0 +1,15 @@
+const firstName = "Justin"
+const lastName = 'TimberLake'
+const fullName = firstName + ' ' + lastName + ' is a good boy';
+// console.log(fullName)
+const fullName2 = `${firstName} ${lastName} is a good boy`
+// console.log(fullName2)
+const multiLine = 'I love you\n'
+    + 'I miss you\n'
+    + 'I need you'
+const multiLine2 = `
+I love you
+I miss you 
+I need you
+`
+console.log(multiLine2)
